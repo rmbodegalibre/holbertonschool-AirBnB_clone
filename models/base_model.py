ed362077235ae15dad13f3b8c4ad5d4271d9051c
davidperlaza14 <davidperlaza14@gmail.com>
2022-10-27 19:39:34 -0500

    models/

--- models/base_model.py
+++ models/base_model.py
@@ -5,16 +5,29 @@ from datetime import datetime
 
 class BaseModel():
 
-    def __init__(self, id = None, created_at = None, updated_at = None):
-        
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()        
-    
+    def __init__(self, *args, **kwargs):
+        """Initialization of a Base instance.
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
+        """
 
-    
-
-
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()  
+   
+   
     def __str__(self):
         """Returns a human-readable string representation
         of an instance."""

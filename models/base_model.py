246ca6e4facc784bbca84537a92cdf2205868951
rmbodegalibre <rmbodegalibre@gmail.com>
2022-10-29 07:59:19 -0500

    test_base_model.py 08

--- models/base_model.py
+++ models/base_model.py
@@ -61,8 +61,8 @@ class BaseModel():
     def __str__(self):
         """Returns a human-readable string representation
         of an instance."""
-        return "[{}] ({}) {}".format\
-            (type(self).__name__, self.id, self.__dict__)
+        return ("[{}] ({}) {}".
+            format(type(self).__name__, self.id, self.__dict__))
 
     def save(self):
         """

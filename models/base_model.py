5ca7400ff70920b2bac3f766c3c69f8876f8d41d
davidperlaza14 <davidperlaza14@gmail.com>
2022-10-29 13:49:10 -0500

    AirBnB

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-"""class BaseModel"""
+"""Module for Base class
+Contains the Base class for the AirBnB clone console.
+"""
+
 import uuid
 from datetime import datetime
+from models import storage
 
-class BaseModel():
+
+class BaseModel:
+
+    """Class for base model of object hierarchy."""
 
     def __init__(self, *args, **kwargs):
         """Initialization of a Base instance.
@@ -25,19 +32,23 @@ class BaseModel():
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()  
-   
-   
+            self.updated_at = datetime.now()
+            storage.new(self)
+
     def __str__(self):
         """Returns a human-readable string representation
         of an instance."""
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
+
     def save(self):
-        """
-        updates the public instance attribute updated_at with the current datetime
-        """
-        # self.created_at = datetime.now()
+        """Updates the updated_at attribute
+        with the current datetime."""
+
         self.updated_at = datetime.now()
+        storage.save()
+
     def to_dict(self):
         """Returns a dictionary representation of an instance."""
 

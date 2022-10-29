d5b25acad6a47c0e98cc7adcb74d4443dc544a80
rmbodegalibre <rmbodegalibre@gmail.com>
2022-10-29 07:52:45 -0500

    test_base_model.py 06

--- models/base_model.py
+++ models/base_model.py
@@ -26,12 +26,15 @@ class BaseModel():
     to_dict(self): returns a dictionary containing all keys/values of __dict__
     of the instance
     by using self.__dict__, only instance attributes set will be returned
-    a key __class__ must be added to this dictionary with the class name of the object
-    created_at and updated_at must be converted to string object in ISO format:
+    a key __class__ must be added to this dictionary with the class name of
+    the object created_at and updated_at must be converted to string object
+    in ISO format:
     format: %Y-%m-%dT%H:%M:%S.%f (ex: 2017-06-14T22:31:03.285259)
     you can use isoformat() of datetime object
-    This method will be the first piece of the serialization/deserialization process:
-    create a dictionary representation with “simple object type” of our BaseModel
+    This method will be the first piece of the serialization/deserialization
+    process:
+    create a dictionary representation with “simple object type” of our
+    BaseModel
     """
     def __init__(self, *args, **kwargs):
         """Initialization of a Base instance.
@@ -55,20 +58,22 @@ class BaseModel():
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
-
     def __str__(self):
         """Returns a human-readable string representation
         of an instance."""
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format\
+        (type(self).__name__, self.id, self.__dict__)
+
     def save(self):
         """
-        updates the public instance attribute updated_at with the current datetime
+        updates the public instance attribute updated_at with the current
+        datetime
         """
         # self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
     def to_dict(self):
         """Returns a dictionary representation of an instance."""
-
         my_dict = self.__dict__.copy()
         my_dict["__class__"] = type(self).__name__
         my_dict["created_at"] = my_dict["created_at"].isoformat()
